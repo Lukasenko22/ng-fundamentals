c15e5ff631bb334377b0d019e40cd12784145ec8
Lukáš Molčan <lukas.molcan@interway.sk>
2021-11-26 16:09:50 +0100

    Pokracovanie v kurze: styles input, output, events

---
+++ src/app/events/event-thumbnail.component.ts
@@ -0,0 +1,28 @@
+import {Component, EventEmitter, Input, Output} from "@angular/core";
+
+@Component({
+  selector: 'event-thumbnail',
+  template: `
+    <div class="well hoverwell thumbnail">
+      <h2>{{eventDetail.name}}</h2>
+      <div>Date: {{eventDetail.date}}</div>
+      <div>Time: {{eventDetail.time}}</div>
+      <div>Price: \${{eventDetail.price}}</div>
+      <div>
+        <span>Location: {{eventDetail.location.address}}</span>
+        <span class="pad-left">{{eventDetail.location.city}}, {{eventDetail.location.country}}</span>
+      </div>
+    </div>
+  `,
+  styles: [`
+    .pad-left {
+      margin-left: 10px;
+    }
+    .well div {
+      color: #bbb;
+    }
+  `]
+})
+export class EventThumbnailComponent {
+  @Input() eventDetail: any
+}
